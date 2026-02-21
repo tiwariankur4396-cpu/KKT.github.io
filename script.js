3474ce5fb875969e8f131550df93ce5238636425
tiwariankur4396-cpu <tiwariankur4396@gmail.com>
2026-02-22 03:15:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ const users = [
 // FIXED: Removed "KKT.github.io/" from paths. 
 // These must be relative to your index.html.
 const files = {
-    screenshot: "files/Screenshot3.png",
-    photo: "files/photo.png",
-    notes: "files/notes.pdf"
+    screenshot: "files/files/Screenshot3.png",
+    photo: "files/files/photo.png",
+    notes: "files/files/notes.pdf"
 };
 
 /* ===== LOGIN FUNCTION ===== */
@@ -66,5 +66,6 @@ function search() {
         result.appendChild(img);
     }
 }
+
 
 

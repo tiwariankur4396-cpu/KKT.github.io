9937d224871497195e765e1d27c95bd67aea0651
tiwariankur4396-cpu <tiwariankur4396@gmail.com>
2026-02-21 20:23:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const users = [
 // REGISTERED FILES
 const files = {
     "notes": "files/notes.pdf",
-    "Screenshot (3)": "files/Screenshot%20(3).png",
+    "Screenshot3": "files/Screenshot3.png",
     "photo": "files/photo.png"
 };
 
@@ -53,6 +53,7 @@ function search() {
         result.innerText = "Not Available";
     }
 }
+
 
 
 
